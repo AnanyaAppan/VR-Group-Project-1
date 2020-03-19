39b1c54491470e5a7ae7e28dd83129174804e2a0
swastishreya <mishra.swastishreya13@gmail.com>
2020-03-20 00:54:37 +0530

    Modified Swasti's implementation of vlad (not yet working)

--- VLAD-BVOW/vlad.py
+++ VLAD-BVOW/vlad.py
@@ -1,93 +1,72 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import keras
-from keras.datasets import cifar10
-import numpy as np 
-import itertools
-from sklearn.cluster import KMeans
-from sklearn.neighbors import BallTree
-import pickle
-import glob
+import os
+import numpy as np
 import cv2
-
-
-# In[2]:
-
-
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-
-print(x_train[0].shape)
-# print(x_test[0])
-
+from sklearn.cluster import KMeans
+from sklearn.model_selection import StratifiedKFold   
+from sklearn.neighbors import BallTree
 
 def desSIFT(image):
     sift = cv2.xfeatures2d.SIFT_create()
-    kp, des = sift.detectAndCompute(image,None)
-    #draw keypoints
-    #import matplotlib.pyplot as plt		
-    #img2 = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
-    #plt.imshow(img2),plt.show()
-    return kp,des
+    keypoints, descriptors = sift.detectAndCompute(image,None)
+    return keypoints, descriptors
 
-def describeORB( image):
-    #An efficient alternative to SIFT or SURF
-    #doc http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_orb/py_orb.html
-    #ORB is basically a fusion of FAST keypoint detector and BRIEF descriptor 
-    #with many modifications to enhance the performance
-    orb=cv2.ORB_create()
-    kp, des=orb.detectAndCompute(image,None)
-    return kp,des
+def describeORB(image):
+    orb = cv2.ORB_create()
+    keypoints, descriptors = orb.detectAndCompute(image,None)
+    return keypoints, descriptors
 
+def features(image, extractor):
+    keypoints, descriptors = extractor.detectAndCompute(image, None)
+    return keypoints, descriptors
 
-# In[60]:
+def read_images(path, folders):
+    data = {}
+    for folder in folders:
+        images = []
+        for filename in os.listdir(path+folder):
+            image = cv2.imread(os.path.join(path+folder, filename))
+            if image is not None:
+                images.append(image)
+        images = np.array(images)
+        data[folder] = images
+    return data
 
+def create_descriptors(data):
+    descriptor_list = []
+    image_descriptor = {}
+    for class_label in data:
+        print(class_label)
+        class_descriptor_list = []
+        for image in data[class_label]:
+            gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            keypoint, descriptor = desSIFT(gray_image)
+            if descriptor is not None:
+                descriptor_list.extend(descriptor)
+                class_descriptor_list.append(descriptor)
+        class_descriptor_list = np.array(class_descriptor_list)
+        image_descriptor[class_label] = class_descriptor_list
+    descriptor_list = np.array(descriptor_list)
+    return descriptor_list, image_descriptor
 
-def getDescriptors(images) : 
-    descriptors = []
-    
-    for image in images : 
-        print (image.shape)
-        kp, des = desSIFT(image)
-        if des is not None : 
-            descriptors.append(des)
-            
-    descriptors = list(itertools.chain.from_iterable(descriptors))
-    descriptors = np.asarray(descriptors)
-        
-    return descriptors
-
-def getVLADDescriptors(images, images_lables, visualDic):
-    descriptors = []
+def getVLADDescriptors(image_descriptor, data_label, visualDic):
+    descriptor = []
     labels = []
-    
-    count = 0
-    for image in images : 
-        kp, des = desSIFT(image)
-        if des is not None : 
-            v = VLAD(des, visualDic)
-            descriptors.append(v)
-            labels.append(images_lables[count])
-        count += 1
-            
-            
-    descriptors = list(itertools.chain.from_iterable(descriptors))
-    descriptors = np.asarray(descriptors)
-        
-    return descriptors, labels
-    
-def kMeans(training, k) : 
-    est = KMeans(n_clusters = k, init = 'k-means++').fit(training)
-    return est
 
-def VLAD(X, visualDictionary) : 
+    # print(image_descriptor)
+    for class_label in image_descriptor:
+        print(class_label)
+        for desc in image_descriptor[class_label]:
+            # print(desc)
+            v = VLAD(desc, visualDic)
+            descriptors.append(v)
+            labels.append([data_label[class_label]])
+    
+    descriptors = np.array(descriptors)
+    labels = np.array(labels).astype(np.float32)
+
+    return descriptors, labels
+
+def VLAD(X, visualDictionary): 
     
     predictedLabels = visualDictionary.predict(X)
     centers = visualDictionary.cluster_centers_
@@ -95,7 +74,7 @@ def VLAD(X, visualDictionary) :
     k = visualDictionary.n_clusters
     
     m,d = X.shape
-    V=np.zeros([k,d])
+    V = np.zeros([k,d])
     #computing the differences
 
     # for all the clusters (visual words)
@@ -103,7 +82,7 @@ def VLAD(X, visualDictionary) :
         # if there is at least one descriptor in that cluster
         if np.sum(predictedLabels==i)>0:
             # add the diferences
-            V[i]=np.sum(X[predictedLabels==i,:]-centers[i],axis=0)
+            V[i] = np.sum(X[predictedLabels==i,:]-centers[i],axis=0)
     
 
     V = V.flatten()
@@ -115,38 +94,54 @@ def VLAD(X, visualDictionary) :
     V = V/np.sqrt(np.dot(V,V))
     return V
 
+data = read_images("../../../Assignment2/Panorama-BOVW/SIFT-SURF/",["Bikes", "Horses"])
+# data = read_images("../../../Assignment2/Panorama-BOVW/SIFT-SURF/cifar-10/",["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"])
+print(data)
+descriptor_list, image_descriptor = create_descriptors(data)
 
-# In[ ]:
+visDic = KMeans(descriptor_list, 50)
 
+data_label = {}
 
-sift_des = getDescriptors(np.concatenate((x_train, x_test), axis = 0))
-visDic = kMeans(sift_des, 500)
+for label in data:
+    if label not in data_label:
+        data_label[label] = len(data_label)
 
+X = []
+y = []
 
-# In[64]:
+for class_label in data:
+    for image in data[class_label]:
+        X.append(image)
+        y.append(class_label)
 
+X = np.asarray(X)
+y = np.asarray(y)
 
-vlad_des, labels = getVLADDescriptors(x_train, y_train, visDic)
-print ("Hola")
-vlad_des_test, labels_test = getVLADDescriptors(x_test, y_test, visDic)
+cv = StratifiedKFold(n_splits=6, random_state=42)
 
+scores = []
+count = 0
+for train_ind, validate_ind in cv.split(X, y):
+    print("CV # - ", count)
+    count += 1
+    train_X, train_y = X[train_ind], y[train_ind]
+    validate_X, validate_y = X[validate_ind], y[validate_ind]
+    descriptor_list, image_descriptor = create_descriptors(train_X)
+    vlad_des, labels = getVLADDescriptors(image_descriptor, data_label, visDic)
+    print ("Hola")
+    descriptor_list, image_descriptor = create_descriptors(validate_X)
+    vlad_des_test, labels_test = getVLADDescriptors(image_descriptor, data_label, visDic)
+    clf = cv2.ml.KNearest_create()
+    clf.train(vlad_des, cv2.ml.ROW_SAMPLE, labels)
+    ret, results, neighbours ,dist = clf.findNearest(vlad_des_test, k=10)
+    print (results)
+    print (labels_test)
+    # score = md.score(validate_X, validate_y)
+    # scores.append(score)
 
-# In[ ]:
+# sc = np.array(scores)
+# print(sc)
+# print("Score: " + str(np.mean(sc)))
 
-
-clf = cv2.ml.KNearest_create()
-clf.train(vlad_des, cv2.ml.ROW_SAMPLE, np.asarray(labels, dtype=np.float32))
-
-
-# In[27]:
-
-
-ret, results, neighbours ,dist = clf.findNearest(vlad_des_test, k=10)
-
-
-# In[ ]:
-
-
-print (results)
-print (labels_test)
 
