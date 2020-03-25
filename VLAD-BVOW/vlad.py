1a7592a2fa0279aa31c44a81e8561a89add855e8
swastishreya <mishra.swastishreya13@gmail.com>
2020-03-25 19:03:15 +0530

    vlad (swasti) modified

--- VLAD-BVOW/vlad.py
+++ VLAD-BVOW/vlad.py
@@ -1,21 +1,14 @@
 import os
 import numpy as np
 import cv2
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.model_selection import StratifiedKFold   
-from sklearn.neighbors import BallTree
-<<<<<<< HEAD
-=======
-import pickle
-import glob
 import cv2
 
+"""
 nb_train_samples = 50000 # 3000 training samples
 nb_valid_samples = 10000 # 100 validation samples
 num_classes = 10
-
-
-# In[2]:
 
 def load_cifar10_data(img_rows, img_cols):
 
@@ -35,8 +28,7 @@ def load_cifar10_data(img_rows, img_cols):
 
 (x_train, y_train ,x_test, y_test) = load_cifar10_data(150,150)
 print("imported dataset")
-
->>>>>>> 0749ae32714cc0a693f6ef96007e6ad30993d231
+"""
 
 def desSIFT(image):
     sift = cv2.xfeatures2d.SIFT_create()
@@ -64,35 +56,29 @@ def read_images(path, folders):
         data[folder] = images
     return data
 
-def create_descriptors(data):
+def create_descriptors(data, data_label):
     descriptor_list = []
-    image_descriptor = {}
+    labels_list = []
     for class_label in data:
-        print(class_label)
-        class_descriptor_list = []
         for image in data[class_label]:
             gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
             keypoint, descriptor = desSIFT(gray_image)
             if descriptor is not None:
                 descriptor_list.extend(descriptor)
-                class_descriptor_list.append(descriptor)
-        class_descriptor_list = np.array(class_descriptor_list)
-        image_descriptor[class_label] = class_descriptor_list
+                labels_list.extend([data_label[class_label] for _ in range(len(descriptor))])
     descriptor_list = np.array(descriptor_list)
-    return descriptor_list, image_descriptor
+    labels_list = np.array(labels_list)
+    return descriptor_list, labels_list
 
-def getVLADDescriptors(image_descriptor, data_label, visualDic):
-    descriptor = []
+def getVLADDescriptors(descriptor_list, labels_list, visualDic):
+    descriptors = []
     labels = []
 
-    # print(image_descriptor)
-    for class_label in image_descriptor:
-        print(class_label)
-        for desc in image_descriptor[class_label]:
-            # print(desc)
-            v = VLAD(desc, visualDic)
-            descriptors.append(v)
-            labels.append([data_label[class_label]])
+    for var in range(len(descriptor_list)):
+        # print(desc)
+        v = VLAD(descriptor_list[var], visualDic)
+        descriptors.append(v)
+        labels.append([labels_list[var]])
     
     descriptors = np.array(descriptors)
     labels = np.array(labels).astype(np.float32)
@@ -129,27 +115,20 @@ def VLAD(X, visualDictionary):
 
 data = read_images("../../../Assignment2/Panorama-BOVW/SIFT-SURF/",["Bikes", "Horses"])
 # data = read_images("../../../Assignment2/Panorama-BOVW/SIFT-SURF/cifar-10/",["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"])
-print(data)
-descriptor_list, image_descriptor = create_descriptors(data)
-
-visDic = KMeans(descriptor_list, 50)
-
 data_label = {}
 
 for label in data:
     if label not in data_label:
         data_label[label] = len(data_label)
 
-X = []
-y = []
+descriptor_list, labels_list = create_descriptors(data, data_label)
+print(descriptors.shape)
+visDic = MiniBatchKMeans(init='k-means++', n_clusters=50,max_iter=1000, batch_size=1000, n_init=10, max_no_improvement=10, verbose=0).fit(descriptor_list)
 
-for class_label in data:
-    for image in data[class_label]:
-        X.append(image)
-        y.append(class_label)
-
-X = np.asarray(X)
-y = np.asarray(y)
+X = np.array(descriptor_list).astype(int)
+print(descriptor_list[0])
+y = labels_list
+print(labels_list[0])
 
 cv = StratifiedKFold(n_splits=6, random_state=42)
 
@@ -160,61 +139,13 @@ for train_ind, validate_ind in cv.split(X, y):
     count += 1
     train_X, train_y = X[train_ind], y[train_ind]
     validate_X, validate_y = X[validate_ind], y[validate_ind]
-    descriptor_list, image_descriptor = create_descriptors(train_X)
-    vlad_des, labels = getVLADDescriptors(image_descriptor, data_label, visDic)
+    
+    vlad_des, labels = getVLADDescriptors(train_X, train_y, visDic)
     print ("Hola")
-    descriptor_list, image_descriptor = create_descriptors(validate_X)
-    vlad_des_test, labels_test = getVLADDescriptors(image_descriptor, data_label, visDic)
+
+    vlad_des_test, labels_test = getVLADDescriptors(validate_X, validate_y, visDic)
     clf = cv2.ml.KNearest_create()
     clf.train(vlad_des, cv2.ml.ROW_SAMPLE, labels)
     ret, results, neighbours ,dist = clf.findNearest(vlad_des_test, k=10)
     print (results)
     print (labels_test)
-    # score = md.score(validate_X, validate_y)
-    # scores.append(score)
-
-# sc = np.array(scores)
-# print(sc)
-# print("Score: " + str(np.mean(sc)))
-
-<<<<<<< HEAD
-=======
-# In[ ]:
-
-
-sift_des = getDescriptors(np.concatenate((x_train, x_test), axis = 0))
-print("got sift descriptors")
-visDic = kMeans(sift_des, 50)
-print("did kmeans")
-
-
-# In[64]:
-
-
-vlad_des, labels = getVLADDescriptors(x_train, y_train, visDic)
-print ("Hola")
-vlad_des_test, labels_test = getVLADDescriptors(x_test, y_test, visDic)
-print("got vlad descriptors")
-
-# In[ ]:
-
-
-clf = cv2.ml.KNearest_create()
-print("created KNN module")
-clf.train(vlad_des, cv2.ml.ROW_SAMPLE, np.asarray(labels, dtype=np.float32))
-print("trainig KNN on ciphar 10")
-
-
-# In[27]:
-
-
-ret, results, neighbours ,dist = clf.findNearest(vlad_des_test, k=10)
-
-
-# In[ ]:
-
-
-print (results)
-print (labels_test)
->>>>>>> 0749ae32714cc0a693f6ef96007e6ad30993d231
-
